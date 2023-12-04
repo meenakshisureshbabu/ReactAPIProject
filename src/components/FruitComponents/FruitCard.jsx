85d110917c545ac3e7cbc01d43570e0c1630dbde
meenakshisureshbabu <meenakshi.bca@gmail.com>
2023-12-03 21:54:43 -0500

    Made an API call code

--- src/components/FruitComponents/FruitCard.jsx
+++ src/components/FruitComponents/FruitCard.jsx
@@ -6,32 +6,36 @@ import '../FruitComponents/fruitcard.css'
 function FruitCard() {
 
   const id = 52;
-  const [fruitdata,setFruitdata] = useState();
-  const apiurl = `https://www.fruityvice.com/api/fruit/${id}`;
-
-  const getFruitData = async() => {
-    const response = await fetch(apiurl);
-    const data = await response.json();
-    setFruitdata(data);
-  }
+  const [data,setData] = useState([]);
+  const apiurl = `/api/fruit/${id}`;
+  
+  const getFruitData = async () => {
+    const resp = await fetch(apiurl);
+    const data = await resp.json();
+    console.log(data);
+    setData(data);
+  };
+  
   useEffect(() => {
-    getFruitData()
-  })
+    console.log("Inside useEffect")
+    getFruitData();
+  });
 
   return (
     <div className='fruit-card-div'>
         <div className="inner-card-div">
           <span className='"close'><IoIosCloseCircle /></span>
             <div className='fruit-header'>
-                <div><h3>Mango</h3></div>
-                <div>Family: Anacardiaceae</div>
+
+                <div><h3>{data.name}</h3></div>
+                <div>Family: {data.family}</div>
                 <div className='nutrients-header-div'>
                     <div><h4>Nutrients</h4></div>
-                    <div>Calories: 60</div>
-                    <div>Fat: 0.38</div>
-                    <div>Sugar: 13.7</div>
-                    <div>Carbohydrates: 11.4</div>
-                    <div>Protein: 0.4</div>
+                    <div>Calories: {data && data.nutritions && data.nutritions.calories}</div>
+                    <div>Fat: {data && data.nutritions && data.nutritions.fat}</div>
+                    <div>Sugar: {data && data.nutritions && data.nutritions.sugar}</div>
+                    <div>Carbohydrates: {data && data.nutritions && data.nutritions.carbohydrates}</div>
+                    <div>Protein: {data && data.nutritions && data.nutritions.protein}</div>
                 </div>
 
             </div>
