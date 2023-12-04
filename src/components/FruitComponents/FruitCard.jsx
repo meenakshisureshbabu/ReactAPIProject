99a59dcfb880547ecb98069c49363466d3c9a3fe
meenakshisureshbabu <meenakshi.bca@gmail.com>
2023-12-03 22:11:42 -0500

    Installed react popup

--- src/components/FruitComponents/FruitCard.jsx
+++ src/components/FruitComponents/FruitCard.jsx
@@ -27,15 +27,15 @@ function FruitCard() {
           <span className='"close'><IoIosCloseCircle /></span>
             <div className='fruit-header'>
 
-                <div><h3>{data.name}</h3></div>
-                <div>Family: {data.family}</div>
+                <div><h2 className='dataname'>{data.name}</h2></div>
+                <div><p className='familyname'>Family: {data.family}</p></div>
                 <div className='nutrients-header-div'>
-                    <div><h4>Nutrients</h4></div>
-                    <div>Calories: {data && data.nutritions && data.nutritions.calories}</div>
-                    <div>Fat: {data && data.nutritions && data.nutritions.fat}</div>
-                    <div>Sugar: {data && data.nutritions && data.nutritions.sugar}</div>
-                    <div>Carbohydrates: {data && data.nutritions && data.nutritions.carbohydrates}</div>
-                    <div>Protein: {data && data.nutritions && data.nutritions.protein}</div>
+                    <div className='nutrients'><h4>Nutrients</h4></div>
+                    <div className='calories'>Calories: <div>{data && data.nutritions && data.nutritions.calories}</div></div>
+                    <div className='fat'>Fat: <div>{data && data.nutritions && data.nutritions.fat}</div></div>
+                    <div className='sugar'>Sugar: <div>{data && data.nutritions && data.nutritions.sugar}</div></div>
+                    <div className='carbo'>Carbohydrates: <div>{data && data.nutritions && data.nutritions.carbohydrates}</div></div>
+                    <div className='protein'>Protein: <div>{data && data.nutritions && data.nutritions.protein}</div></div>
                 </div>
 
             </div>
